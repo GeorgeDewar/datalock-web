5dfb62a5efcc2906b200f15bf578819057ec157f
Owen Bannister <ob2640@gmail.com>
2014-07-26 00:59:21 +1200

    messages controller poc

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,12 @@
 class MessagesController < ApplicationController
 
   def poll
-    render :text => "unlock"
+    string = Message.next_message.to_package
+    render :text => string
+  end
+
+  def arduino_response
+
   end
 
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,20 +1,35 @@
 class Message < ActiveRecord::Base
   belongs_to :command
 
-  def unlock
+  scope :next_message, -> { where(confirmed_at: nil).order(:created_at).first }
 
+  def to_package
+    msg = ""
+    if command.code == "UNL"
+      msg = unlock
+    elsif command.code == "LOK"
+      msg = lock
+    elsif command.code == "USR"
+      msg = add_edit_user
+    elsif command.code == "REM"
+      msg = remove_user
+    end
+  end
+
+  private 
+  def unlock
+    "UNLOCKING"
   end
 
   def lock 
-
+    "LOCKING"
   end
 
   def add_edit_user
-
+    "ADDING USER"
   end  
 
   def remove_user
-
+    "REMOVING USER"
   end
-  
 end
