29c1ac0e71811c4993a77375e4950935d2de93e2
Owen Bannister <ob2640@gmail.com>
2014-07-25 23:10:45 +1200

    Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,8 @@ door = Door.create(name: "Front Door")
 admin_user = User.create(name: "Dad", admin: true, pin: "1234", email: "dad@gmail.com", password: "password", password_confirmation: "password")
 user = User.create(name: "Child", admin: false, pin: "1234", email: "child@gmail.com", password: "password", password_confirmation: "password")
 temp_user = TempUser.create(name: "Plumber", pin: "1234", expiry_at: Time.now+1.day)
+
+unlock_command = Command.create(code: "UNL", name: "Unlock")
+lock_command = Command.create(code: "LOK", name: "Lock")
+add_edit_user = Command.create(code: "USR", name: "Add/Edit User")
+remove_user = Command.create(code: "REM", name: "Remove User")
