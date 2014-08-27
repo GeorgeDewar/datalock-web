f84622a1d855226080c3a2ddc9ca31e424d96eb2
Owen Bannister <ob2640@gmail.com>
2014-08-27 15:18:02 +1200

    default admin to false

--- db/migrate/20140725064047_devise_create_users.rb
+++ db/migrate/20140725064047_devise_create_users.rb
@@ -19,7 +19,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.string   :current_sign_in_ip
       t.string   :last_sign_in_ip
       t.text     :name
-      t.boolean  :admin
+      t.boolean  :admin, default: false, null: false
       t.text     :pin
       t.text     :ph_number 
       ## Confirmable
