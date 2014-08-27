dc414ea782dd96126f6e843ce12aa0e86f6b4d8f
Owen Bannister <ob2640@gmail.com>
2014-08-27 14:30:37 +1200

    Display event times

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,9 @@ module DatalockWeb
     config.autoload_paths += %W(
       #{config.root}/lib
     )
+    config.time_zone = 'Wellington'
+    config.active_record.default_timezone = :local
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
